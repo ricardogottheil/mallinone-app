77592fbf3019f9212aa703d664d58ff26bfc12a0
Ricardo <ricardogottheil@gmail.com>
2018-04-07 19:39:39 -0500

    Agregando la vista del mapa y su API

--- Android/MallInOne/app/src/main/java/mallinone/ricardogotheil/com/mallinone/MainActivity.kt
+++ Android/MallInOne/app/src/main/java/mallinone/ricardogotheil/com/mallinone/MainActivity.kt
@@ -1,12 +1,20 @@
 package mallinone.ricardogotheil.com.mallinone
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonGoogleMaps.setOnClickListener {
+            val mapa = Intent(this, MapsActivity::class.java)
+            startActivity(mapa)
+
+        }
     }
 }
