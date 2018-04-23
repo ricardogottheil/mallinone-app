cd6b7548fd081bead3263df22dcdac81eef6031d
Ricardo Gottheil <ricardogottheil@gmail.com>
2018-04-23 08:11:03 -0500

    Se creó el usuario para entrar al administrador de Django

--- WEB/MallInOne/MallInOne/urls.py
+++ WEB/MallInOne/MallInOne/urls.py
@@ -13,6 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
+"""
+Para ingresar al administrador de django usar...
+
+username: admin
+password: superuser
+
+"""
 from django.conf.urls import url
 from django.contrib import admin
 
