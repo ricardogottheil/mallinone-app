885547034ab6202e3e6ac89eeb9972fe4f76a440
Ricardo Gottheil <ricardogottheil@gmail.com>
2018-04-23 08:49:06 -0500

    Inicio de la estructura de un Mall/CentroComercial

--- WEB/MallInOne/MallInOne/urls.py
+++ WEB/MallInOne/MallInOne/urls.py
@@ -24,6 +24,9 @@ password: superuser
 from django.conf.urls import url
 from django.contrib import admin
 
+from mall.views import index
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', index),
 ]
--- WEB/MallInOne/mall/models.py
+++ WEB/MallInOne/mall/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Mall(models.Model):
+  name = models.CharField(max_length=120)
+
+  def __str__(self):
+    return self.name
