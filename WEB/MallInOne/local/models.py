a01bec353bfc98f90c4dd52afe36998614ed5954
Ricardo Gottheil <ricardogottheil@gmail.com>
2018-04-23 16:48:50 -0500

    Se agrego la asociacion entre el local y el mall

--- WEB/MallInOne/local/models.py
+++ WEB/MallInOne/local/models.py
@@ -3,9 +3,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+# from product.models import Product
+
 # Create your models here.
 class Local(models.Model):
-  name = models.CharField(max_length=120)
+  name       = models.CharField(max_length=120)
+  # product    = models.ForeignKey(Product, null=True)
 
   def __str__(self):
     return self.name
--- WEB/MallInOne/mall/models.py
+++ WEB/MallInOne/mall/models.py
@@ -2,10 +2,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from local.models import Local
 
 # Create your models here.
 class Mall(models.Model):
-  name = models.CharField(max_length=120)
+  name       = models.CharField(max_length=120)
+  local      = models.ForeignKey(Local, null=True)
 
   def __str__(self):
     return self.name
