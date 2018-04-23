bf3713ecaa850dd8d0eff38d30cb85e1f14167dd
Ricardo Gottheil <ricardogottheil@gmail.com>
2018-04-23 09:32:20 -0500

    Inicio de la estructura de un Producto

--- WEB/MallInOne/product/models.py
+++ WEB/MallInOne/product/models.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+  name      = models.CharField(max_length=120)
+  reference = models.CharField(max_length=120, null=True, blank=True)
+  precio    = models.CharField(max_length=120, null=True, blank=True)
+
+  def __str__(self):
+    return self.name
