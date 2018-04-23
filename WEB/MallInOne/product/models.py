b7e5f2ea41b758bbc7a8ffaff0f6463f93cded26
Ricardo Gottheil <ricardogottheil@gmail.com>
2018-04-23 16:19:20 -0500

    Se agregó la asociación entre producto y el local

--- WEB/MallInOne/product/models.py
+++ WEB/MallInOne/product/models.py
@@ -3,11 +3,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from local.models import Local
+
+
 # Create your models here.
 class Product(models.Model):
-  name      = models.CharField(max_length=120)
-  reference = models.CharField(max_length=120, null=True, blank=True)
-  precio    = models.CharField(max_length=120, null=True, blank=True)
+  local              = models.ForeignKey(Local, null=True) 
+  name               = models.CharField(max_length=120)
+  reference          = models.CharField(max_length=120, null=True, blank=True)
+  price              = models.CharField(max_length=120, null=True, blank=True)
+  brand              = models.CharField(max_length=120, null=True, blank=True)
+  characteristics    = models.TextField(null=True, blank=True)
 
   def __str__(self):
     return self.name
