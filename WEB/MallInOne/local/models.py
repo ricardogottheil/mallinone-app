8c24c92c7ad6c001ebedb5eef90169f7cdc422bb
Ricardo Gottheil <ricardogottheil@gmail.com>
2018-05-18 21:09:39 -0500

    Se agregan campos y se organiza los modelos de cada app

--- WEB/MallInOne/local/models.py
+++ WEB/MallInOne/local/models.py
@@ -1,17 +1,20 @@
 from __future__ import unicode_literals
 from django.db import models
 from pygments import highlight
-from product.models import Product
+#from product.models import Product
+from mall.models import Mall
 
 class Local(models.Model):
   id         = models.IntegerField(primary_key=True, default=0)
   name       = models.CharField(max_length=120)
-  product    = models.ManyToManyField(Product, related_name='%(class)s_Local')
+  mall      = models.ForeignKey(Mall, related_name='%(class)s_Local', null=True)
+  #product    = models.ManyToManyField(Product, related_name='%(class)s_Local')
   owner = models.ForeignKey('auth.User', related_name='%(class)s_Local', on_delete=models.CASCADE, default='')
-  highlighted = models.TextField(default='')
-  image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100, default='')
-  latitude = models.IntegerField(default=0)
-  longitude = models.IntegerField(default=0)
+  highlighted = models.TextField(default='', null=True, blank=True)
+  image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100, default='', null=True, blank=True)
+  map_url = models.CharField(max_length=300, default='', null=True, blank=True)
+  # latitude = models.IntegerField(default=0)
+  # longitude = models.IntegerField(default=0)
 
   def __unicode__(self):
     return self.name
--- WEB/MallInOne/product/models.py
+++ WEB/MallInOne/product/models.py
@@ -1,17 +1,19 @@
 from __future__ import unicode_literals
 from django.db import models
 from pygments import highlight
+from local.models import Local
 
 # Create your models here.
 class Product(models.Model):
   id                 = models.IntegerField(primary_key=True, default=0)
+  local              = models.ForeignKey(Local, related_name='%(class)s_Product', null=True)
   name               = models.CharField(max_length=120)
   price              = models.CharField(max_length=120, null=True, blank=True)
   brand              = models.CharField(max_length=120, null=True, blank=True)
   characteristics    = models.TextField(null=True, blank=True)
   owner              = models.ForeignKey('auth.User', related_name='%(class)s_Product', on_delete=models.CASCADE, default='')
-  highlighted        = models.TextField(default='')
-  image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100, default='')
+  highlighted        = models.TextField(default='', blank=True)
+  image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100, default='', blank=True)
 
   def __unicode__(self):
     return self.name
